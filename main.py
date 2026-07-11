66d887687c3f0e6ec682f328686812067d2e4807
Stas_Untitled <nasicomruutubery@gmail.com>
2026-07-11 11:53:01 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,29 @@
-def main():
-    print("Hello from wanwanbot!")
+import asyncio
+from os import getenv
 
+from aiogram import Bot, Dispatcher
+from aiogram.types import Message
+
+dp = Dispatcher()
+
+@dp.message()
+async def any_message(
+		message: Message,
+):
+	await message.answer("wanwan!")
+
+async def main():
+	token = getenv("BOT_TOKEN")
+	if not token:
+		error = "No token provided"
+		raise ValueError(error)
+	bot = Bot(token=token)
+
+	print("Starting bot...")
+	try:
+		await dp.start_polling(bot)
+	finally:
+		print("Bot stopped")
 
 if __name__ == "__main__":
-    main()
+	asyncio.run(main())
